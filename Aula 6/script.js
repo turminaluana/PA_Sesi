cccd0b4df3576df8440c0cbd67e99a97c6f4dad1
luana <turminaluana@gmail.com>
2026-05-26 09:14:00 -0300

    aula 6

---
+++ Aula 6/script.js
@@ -0,0 +1,24 @@
+console.log(5+5)
+console.log(5+"5")
+
+let aluno = "Davi"
+var  aluno_var = "Lucas"
+const nome = "Pietro"  //não muda
+
+console.log("O " + nome + " é aluno do sesi")
+
+console.log(aluno_var)
+
+aluno_var = "Vinicios"
+
+console.log(aluno_var)
+
+let frutas = ["Maçã", "Uva", "Pêra"]
+
+console.log(frutas.length)
+
+let nomes = { 
+    id: 1,
+    nome: "Brasil",
+    senha: 123
+}
