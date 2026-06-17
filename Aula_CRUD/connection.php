9ee234c67a4af1dac41ab68f3d1ee8af822da716
luana <turminaluana@gmail.com>
2026-06-17 10:27:58 -0300

    crud

--- Aula_CRUD/connection.php
+++ Aula_CRUD/connection.php
@@ -5,3 +5,14 @@ $user = "root";
 $password = "root";
 $database = "aula_sesi";
 $port = 3307;
+
+
+try{
+    $conn = mysqli_connect($host, $user, $password, $database, $port);
+    echo "Conectado com o banco de dados MySQL";
+
+}
+
+catch(Exception $e){
+    echo $e;
+}
